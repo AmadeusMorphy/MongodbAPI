59b1e115c6d94f6fa6df20e857b6b75009bdc74d
Nizar Masadeh <n.masadeh@futeric.com>
2024-10-13 11:49:35 +0300

    fix 2

--- api/users.js
+++ api/users.js
@@ -6,12 +6,16 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection string
-const MONGO_URI = 'mongodb+srv://nizarmasadeh2001:nizaR123@users.dbgaj.mongodb.net/?retryWrites=true&w=majority&appName=users';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb+srv://nizarmasadeh2001:nizaR123@users.dbgaj.mongodb.net/?retryWrites=true&w=majority&appName=users';
 
 // Connect to MongoDB Atlas
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected!'))
-    .catch(err => console.error('MongoDB connection error:', err));
+    .then(() => {
+        console.log('MongoDB connected!');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 // Define a simple schema for your data
 const userSchema = new mongoose.Schema({
